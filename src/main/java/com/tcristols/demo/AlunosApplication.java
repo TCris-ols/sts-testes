91ba13532f25eefb2c47c612ab62833d75e591a3
Teresa Cristina O Souza <teresa.cristols@gmail.com>
2021-02-02 09:11:15 -0300

    first commit

---
+++ src/main/java/com/tcristols/demo/AlunosApplication.java
@@ -0,0 +1,13 @@
+package com.tcristols.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunosApplication.class, args);
+	}
+
+}
